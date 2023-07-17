79d0b57f5db74354b66eaa61d142d51afdfd6e45
Bartgeier <38810589+bartgeier@users.noreply.github.com>
2023-07-17 15:42:07 +0200

    added unittests for TextFile

--- unitTests/test_TextFile.cpp
+++ unitTests/test_TextFile.cpp
@@ -11,14 +11,14 @@ TEST(TextFile, load) {
         bool error = false;
         error = txt.load(path.c_str(), error);
         ASSERT_FALSE(error);
-        EXPECT_EQ(txt.file.size(), 71);
-        EXPECT_EQ(txt.lines.size(), 5);
+        EXPECT_EQ(txt.file.size(), 87);
+        EXPECT_EQ(txt.lines.size(), 7);
         EXPECT_EQ(txt.lines[0].start, 0);
         EXPECT_EQ(txt.lines[0].end, 18);
         EXPECT_EQ(txt.lines[1].start, 19);
         EXPECT_EQ(txt.lines[1].end, 19);
-        EXPECT_EQ(txt.lines[txt.lines.size() - 1].start, 52);
-        EXPECT_EQ(txt.lines[txt.lines.size() - 1].end, 70);
+        EXPECT_EQ(txt.lines[txt.lines.size() - 1].start, 85);
+        EXPECT_EQ(txt.lines[txt.lines.size() - 1].end, 86);
 }
 
 TEST(TextFile, error) {
@@ -31,4 +31,28 @@ TEST(TextFile, error) {
         EXPECT_TRUE(error);
         EXPECT_EQ(txt.file.size(), 0);
         EXPECT_EQ(txt.lines.size(), 0);
+}
+
+TEST(TextFile, first_character_in_line) {
+        std::filesystem::path currentDir = std::filesystem::current_path();
+        std::string path = std::filesystem::current_path();
+        path.append("/../../unitTests/Example.txt");
+        TextFile txt;
+        bool error = false;
+        error = txt.load(path.c_str(), error);
+        ASSERT_FALSE(error);
+        EXPECT_TRUE(txt.first_character_in_line(txt.lines[5].start + 8));
+        EXPECT_FALSE(txt.first_character_in_line(txt.lines[5].start + 9));
+}
+
+TEST(TextFile, last_character_in_line) {
+        std::filesystem::path currentDir = std::filesystem::current_path();
+        std::string path = std::filesystem::current_path();
+        path.append("/../../unitTests/Example.txt");
+        TextFile txt;
+        bool error = false;
+        error = txt.load(path.c_str(), error);
+        ASSERT_FALSE(error);
+        EXPECT_FALSE(txt.last_character_in_line(txt.lines[5].start + 9));
+        EXPECT_TRUE(txt.last_character_in_line(txt.lines[5].start + 11));
 }
